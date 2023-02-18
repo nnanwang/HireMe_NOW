b3ffb78c49440ec7a92d3d7dc0ecd6bba144eb82
Zheng <zhengzheng@brandeis.edu>
2023-02-17 23:06:17 -0500

    update website

--- hmn_project/hire_me_now/views.py
+++ hmn_project/hire_me_now/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+import openai
 
-# Create your views here.
+# Create Homepage
+def home(request):
+    # We want to render a webpage, request, template_name => page name, context => return
+    if request.method == "POST":
+        question = request.POST['question']
+        return render(request=request, template_name='screen/home.html', context={"question":question})
+    
+    return render(request=request, template_name='screen/home.html', context={})
+    
